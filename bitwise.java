ea964cbc6fd63357045426c45ba24aebc62ec78a
madhu11220 <150243904+madhu11220@users.noreply.github.com>
2025-03-18 15:14:52 +0530

    Add files via upload
    session2

---
+++ bitwise.java
@@ -0,0 +1,43 @@
+package programs;
+
+public class bitwise {
+public static void main(String[] args) {
+	 int a = 5, b = 3;
+	 /*
+	  *AND
+	  *0101
+	  *0011
+	  *----
+	  *0001-->1
+	  *----
+	  * 
+	  * OR
+	  * 0101
+	  * 0011
+	  * ----
+	  * 0111-->7
+	  * ----
+	  * 
+	  * XOR
+	  * 0101
+	  * 0011
+	  * ----
+	  * 0110-->6
+	  * ----
+	  * 
+	  * NOT-->-(n+1)
+	  * 0101-->5
+	  * 0011-->1
+	  * ----
+	  * 0110-->6
+	  * ----
+	  * 
+	  * final answer-->-6
+	  */
+	 
+	 System.out.println("Bitwise AND:"+ (a & b));
+	 System.out.println("Bitwise OR:"+ (a | b));
+	 System.out.println("Bitwise XOR:"+ (a ^ b));
+	 System.out.println("Bitwise NOT:"+ (~a));
+}
+}
